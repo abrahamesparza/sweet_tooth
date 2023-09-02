4041c77252638999a6da2402da450baafda68441
Abraham Esparza <abrahamesparza.dev@gmail.com>
2023-09-01 18:52:48 -0700

    Clean unused code and comments

--- frontend/src/root/App.jsx
+++ frontend/src/root/App.jsx
@@ -1,29 +1,15 @@
 import { useState, useEffect } from "react";
 
 import './App.css';
-import Landing from "../components/Landing";
 import Header from "../components/Header";
 import Page from "../components/Page";
 
-// note: all the commented out
-// code below is tied together. 
-
 function App() {
-  const [login, setLogin] = useState(false);
   const [page, setPage] = useState('');
 
   function handleFormClick(page) {
     console.log('text in app', page)
     setPage(page);
-  }
-  
-  function switchForm(e) {
-    e.preventDefault()
-    if (!login) {
-      setLogin(true)
-    } else {
-      setLogin(false)
-    }
   }
 
   return (
@@ -32,12 +18,7 @@ function App() {
 
         <div className='main'>
           <Header onSignUpClick={handleFormClick}/>
-          {/*
-            want to display landing first nd route the user
-            to the page they click from the dropdown.
-            need to figure out logic
-          */}
-          { setPage ? <Page showPage={page} switchForm={switchForm}/> : null}
+          { setPage ? <Page showPage={page} /> : null}
         </div>
       </div>
     </>
